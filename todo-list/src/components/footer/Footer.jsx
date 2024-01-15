a083025bb0ac183c711fa4ed78e345b65d894a55
paolospoto <paolo.spoto@gmail.com>
2024-01-15 11:31:05 +0100

    feat: add git profile link

--- todo-list/src/components/footer/Footer.jsx
+++ todo-list/src/components/footer/Footer.jsx
@@ -3,7 +3,10 @@ import styles from "./index.module.scss";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <img src="/public/git.svg" alt="git-icon" width={25} />
+      <p>See more on my GitHub! </p>
+      <a href="https://github.com/paolospoto" rel="noreferrer" target="_blank">
+        <img src="/public/git.svg" alt="git-icon" width={50} />
+      </a>
     </div>
   );
 };
