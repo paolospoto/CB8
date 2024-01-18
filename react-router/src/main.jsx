68367d5ff1ec9e2257a35c23c9d588f98ffdaac7
paolospoto <paolo.spoto@gmail.com>
2024-01-18 11:06:36 +0100

    esercizio base

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,29 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Page404 from './routes/Page404';
-import Homepage from './routes/homepage';
-import './index.css'
-import Product from './routes/product';
+import Page404 from "./routes/Page404";
+import Homepage from "./routes/homepage";
+import "./index.css";
+import Product from "./routes/product";
+import Contacts from "./routes/contacts";
+import About from "./routes/about";
 
 const router = createBrowserRouter([
   {
     path: "/", // Rotta statica
     element: <Homepage />,
-    errorElement: <Page404 />
+    errorElement: <Page404 />,
+  },
+  {
+    path: "/contacts", // Rotta statica
+    element: <Contacts />,
+    errorElement: <Page404 />,
+  },
+  {
+    path: "/about", // Rotta statica
+    element: <About />,
+    errorElement: <Page404 />,
   },
   {
     path: "products/:productId", // Rotta dinamica
     element: <Product />,
   },
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ react-router/src/routes/contacts.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Contacts = () => {
+  return <div>contacts</div>;
+};
+
+export default Contacts;
