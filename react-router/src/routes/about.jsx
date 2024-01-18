839139dc2ff315e6489b882663a12773ed79e5f7
paolospoto <paolo.spoto@gmail.com>
2024-01-18 14:08:46 +0100

    wp

--- react-router/src/routes/about.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-const About = () => {
-  return <div>about</div>;
-};
-
-export default About;
--- react-router/src/routes/contacts.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-const Contacts = () => {
-  return <div>contacts</div>;
-};
-
-export default Contacts;
